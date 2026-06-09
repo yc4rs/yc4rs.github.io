4d88526c81d818536180e525c5d700059fdf8344
yaochi <yaochi_huang@ruisi.com.tw>
2026-06-09 15:41:12 +0800

    更新：ＤＥＭＯ跟ＳＰＩＮＥＶＩＥＷＥＲ工具

---
+++ other_page/Demo/src/settings.da2a3.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/demo_01.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"3640b"}};
---
+++ other_page/SpineViewer/build/src/settings.c6f7e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/viewer.fire",orientation:"",jsList:[],bundleVers:{internal:"803f8",main:"7b87c"}};
