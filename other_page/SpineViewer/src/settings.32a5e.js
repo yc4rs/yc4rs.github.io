ff4abd5b249959c92b4cafca4ed03c1b53f479ea
yaochi <yaochi_huang@ruisi.com.tw>
2026-06-24 17:35:02 +0800

    更新：SpineViewer

--- other_page/SpineViewer/src/settings.32a5e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/demo_01.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"954df"}};
